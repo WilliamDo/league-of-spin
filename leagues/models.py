03874d00642dec98a54919bcaf5fbc107291880f
William Do <william@ultimaspin.com>
2018-08-23 17:19:08 +0100

    create new model for teams and players with migrations

--- leagues/models.py
+++ leagues/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    team_name = models.CharField(max_length=100)
+
+class Player(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
