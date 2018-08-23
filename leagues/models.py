8b1b95c81802bc2311f5f477f559c9564c620536
William Do <william@ultimaspin.com>
2018-08-23 17:26:40 +0100

    add models to admin and provide string representation

--- leagues/models.py
+++ leagues/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Team(models.Model):
     team_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.team_name
+
 class Player(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
