df8b8e5cf896857c248980d9fa05bcb71241d2aa
William Do <william@ultimaspin.com>
2018-08-24 19:05:28 +0100

    add computed data to model to calculate league table scores

--- leagues/models.py
+++ leagues/models.py
@@ -28,6 +28,13 @@ class Team(models.Model):
     def __str__(self):
         return self.team_name
 
+    def count_matches_for(self):
+        return sum([fixture.get_home_score() for fixture in self.home_fixtures.all()]) + sum([fixture.get_away_score() for fixture in self.away_fixtures.all()])
+
+    
+    def count_matches_against(self):
+        return sum([fixture.get_away_score() for fixture in self.home_fixtures.all()]) + sum([fixture.get_home_score() for fixture in self.away_fixtures.all()])
+
 class Player(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
@@ -45,10 +52,33 @@ class Fixture(models.Model):
     def __str__(self):
         return f'{self.home_team.team_name} vs. {self.away_team.team_name}'
 
+    def get_home_score(self):
+        matches_won = [m for m in self.match_set.all() if m.get_winner() in self.home_team.player_set.all()]
+        return len(matches_won)
+
+    def get_away_score(self):
+        matches_won = [m for m in self.match_set.all() if m.get_winner() in self.away_team.player_set.all()]
+        return len(matches_won)
+
 class Match(models.Model):
     fixture = models.ForeignKey(Fixture, on_delete=models.CASCADE)
     home_player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='home_matches')
     away_player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='away_matches')
+
+    def get_winner(self):
+        home_games = 0
+        away_games = 0
+        for game in self.game_set.all():
+            if game.home_score > game.away_score:
+                home_games += 1
+            else:
+                away_games += 1
+
+        # FIXME This ia a bug because what if games are equal?
+        if home_games > away_games:
+            return self.home_player
+        else:
+            return self.away_player
 
 class Game(models.Model):
     match = models.ForeignKey(Match, on_delete=models.CASCADE)
--- leagues/tests.py
+++ leagues/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
+from .models import Fixture, Match, Team
+
 # Create your tests here.
+
+class FooTests(TestCase):
+    fixtures = ['dev']
+
+    def test_get_winner_home(self):
+        match = Match.objects.get(pk=1)
+        winner = match.get_winner()
+        self.assertEqual(winner.first_name, 'Matt')
+
+    def test_get_winner_away(self):
+        match = Match.objects.get(pk=9)
+        winner = match.get_winner()
+        self.assertEqual(winner.first_name, 'Mike')
+
+    def test_fixture_get_score(self):
+        fixture = Fixture.objects.get(pk=1)
+        self.assertEqual(fixture.get_home_score(), 8)
+        self.assertEqual(fixture.get_away_score(), 1)
+
+    def test_team_count_matches(self):
+        team = Team.objects.get(pk=1)
+        self.assertEqual(team.count_matches_for(), 8)
+        self.assertEqual(team.count_matches_against(), 1)
+
+        team = Team.objects.get(pk=2)
+        self.assertEqual(team.count_matches_for(), 1)
+        self.assertEqual(team.count_matches_against(), 8)
+        
