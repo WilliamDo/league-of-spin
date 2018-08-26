e500be983269e391e26d5632ba6ea236d62e4ccf
William Do <william@ultimaspin.com>
2018-08-26 21:43:59 +0100

    add new view for fixture scorecard

--- leagues/views.py
+++ leagues/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
-from .models import Division
+from .models import Division, Fixture
 
 # Create your views here.
 def index(request):
@@ -12,3 +12,14 @@ def table(request, division_id):
     teams = division.team_set.all()
     sorted_teams = sorted(teams, key=lambda t: t.count_matches_for(), reverse=True)
     return render(request, 'leagues/table.html', {'teams': sorted_teams})
+
+def fixture(request, fixture_id):
+    fixture = get_object_or_404(Fixture, pk=fixture_id)
+    matches = fixture.match_set.all()
+
+    return render(request, 'leagues/fixture.html', {
+        'home_team': fixture.home_team.team_name,
+        'away_team': fixture.away_team.team_name,
+        'fixture': fixture,
+        'matches': matches
+    })
